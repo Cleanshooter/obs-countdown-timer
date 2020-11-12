87ce782f4eff606b2ee1d580013e56bf4a93ccfa
Joe Motacek <cleanshooter@gmail.com>
2020-11-12 09:41:44 -0600

    added material UI

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Typography } from '@material-ui/core';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
+      <AppBar position="static">
+        <Typography variant="h6">OBS Countdown Timer</Typography>
+      </AppBar>
       <h2>Home</h2>
       <Link to={routes.COUNTER}>to Counter</Link>
     </div>
